b44041d05167984fe72ced1d3c015a790371c491
Arjan de Ruijter <rra@mboutrecht.nl>
2016-09-29 11:54:53 +0200

    Initialize

---
+++ content.php
@@ -0,0 +1,1 @@
+Dit is wat content op het scherm
---
+++ link.php
@@ -0,0 +1,5 @@
+<ul>
+    <li><a href='./home.php'>home</a></li>
+    <li><a href='./register_form.php'>registration</a></li>
+    <li><a href='./login_form.php'>login</a></li>    
+</ul>
