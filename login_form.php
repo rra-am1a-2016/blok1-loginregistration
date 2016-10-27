ca5f20766ae31bac45b0793a42292e25c6eb5467
Arjan de Ruijter <rra@mboutrecht.nl>
2016-10-27 09:19:49 +0200

    email is nu uniek

--- login_form.php
+++ login_form.php
@@ -8,15 +8,47 @@
       $email = "";
    }
 
+   if (isset($_POST["submit"]))
+   {
+      echo "Er is op de submitknop gedrukt";
+      var_dump($_POST);
+      // Eerst contact maken met de mysql-server
+      include("./connect_db.php");
+
+      // select query opbouwen
+      $sql = "SELECT * FROM `users` 
+              WHERE `email` = '".$_POST["email"]."'
+              AND   `password`    = '".sha1($_POST["password"])."'";
+
+      //echo $sql;exit();
+
+      // Stuur de query naar de database...
+      $result = mysqli_query($conn, $sql);
+
+      // Tel hoeveel records er zijn gevonden
+      if ( mysqli_num_rows($result) > 0 )
+      {
+
+         // Maak van de resource $result een array (associatief)
+         $record = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+         var_dump($record);
+      }
+   }
+   else
+   {
+      echo "Er is niet gedrukt op de submitknop";
+   
+
 ?>
 
 
 <h3>Voer uw gebruikersnaam (emailadres) en wachtwoord in:</h3>
-<form>
+<form action="./index.php?content=login_form" method="post">
    <table>
       <tr>
          <td>emailadres: </td>
-          <td><input type="text" name="email" required value="<?php echo $email; ?>"></td>
+          <td><input type="email" name="email" required value="<?php echo $email; ?>"></td>
       </tr>
       <tr>
          <td>wachtwoord</td>
@@ -28,3 +60,7 @@
       </tr>
    </table>
 </form>
+
+<?php
+   }
+?>
--- register_form.php
+++ register_form.php
@@ -24,85 +24,102 @@
       $infix = sanitize($_POST["infix"]);
       $lastname = sanitize($_POST["lastname"]);
       $email = sanitize($_POST["email"]);
-      
-      // Maak de insert query..
-      $sql = "INSERT INTO `users` (`id`,
-                                   `firstname`,
-                                   `infix`,
-                                   `lastname`,
-                                   `email`,
-                                   `password`,
-                                   `activate`)
-              VALUES             (NULL,
-                                  '".$firstname."',
-                                  '".$infix."',
-                                  '".$lastname."',
-                                  '".$email."',
-                                  '".$tempPassword."',
-                                  'false')";
-      
-      // Vuur de query af op de database via de verbinding $conn
+
+      // Maak de selectiequery om te controleren of het emailadres al bestaat in de database
+      $sql = "SELECT * FROM `users` WHERE `email` = '".$_POST["email"]."'";
+
       $result = mysqli_query($conn, $sql);
-     
-      // Vraag het id op van het aangemaakt record
-      $last_id = mysqli_insert_id($conn);
 
-      
-      // Als de query correct is ontvangen en uitgevoerd.
-      if ($result)
+      if (mysqli_num_rows($result) == 0)
       {
-         $emailaddress = $_POST["email"];
-         $subject = "Activatie account";
-         
-         /*
-         $message = "Beste ".$firstname." ".$infix." ".$lastname."\n\n".
-                    "Bedankt voor het registreren, klik op onderstaande link\n\n".
-                    "http://localhost/2016-2017/am1a/inlogregistratiesysteem/index.php?content=activate&id=".
-                    $last_id."&pw=".$tempPassword." \n\nom uw account te activeren\n\n". 
-                    "Met vriendelijke groet,\n". 
-                    "admin";
-         */
-         $messageHtml = "<!DOCTYPE html>
-                         <html>
-                           <head>
-                              <title>Page Title</title>
-                              <style>
-                                 body
-                                 {
-                                    font-family: Verdana, Arial;
-                                    font-size: 1em;
-                                    color: rgb(30, 30, 30);
-                                 }
-                              </style>
-                           </head>
-                           <body>
-                           <h3>Beste ".$firstname." ".$infix." ".$lastname.",</h3>".
-                              "<p>Bedankt voor het registreren, klik op onderstaande link<p>".
-                              "<p><a href='http://localhost/2016-2017/am1a/inlogregistratiesysteem/index.php?content=activate&id=".
-                              $last_id."&pw=".$tempPassword."'>activatielink</a></p><p>om uw account te activeren</p>". 
-                              "<p>Met vriendelijke groet,</p>". 
-                              "<p>admin</p>
-                           </body>
-                        </html>";
-         
-         $headers = "Content-Type: text/html; charset=UTF-8"."\r\n";
-         $headers .= "Cc: adruijter@fopmail.com, hans@testmail.com, frans@realmail.com"."\r\n";
-         $headers .= "Bcc: rra@mboutrecht.nl"."\r\n";
-         $headers .= "From: adruijter@gmail.com";
+
+        // Maak de insert query..
+        $sql = "INSERT INTO `users` (`id`,
+                                    `firstname`,
+                                    `infix`,
+                                    `lastname`,
+                                    `email`,
+                                    `password`,
+                                    `activate`)
+                VALUES             (NULL,
+                                    '".$firstname."',
+                                    '".$infix."',
+                                    '".$lastname."',
+                                    '".$email."',
+                                    '".$tempPassword."',
+                                    'false')";
+        
+        // Vuur de query af op de database via de verbinding $conn
+        $result = mysqli_query($conn, $sql);
+      
+        // Vraag het id op van het aangemaakt record
+        $last_id = mysqli_insert_id($conn);
+
+        
+        // Als de query correct is ontvangen en uitgevoerd.
+        if ($result)
+        {
+          $emailaddress = $_POST["email"];
+          $subject = "Activatie account";
+          
+          /*
+          $message = "Beste ".$firstname." ".$infix." ".$lastname."\n\n".
+                      "Bedankt voor het registreren, klik op onderstaande link\n\n".
+                      "http://localhost/2016-2017/am1a/inlogregistratiesysteem/index.php?content=activate&id=".
+                      $last_id."&pw=".$tempPassword." \n\nom uw account te activeren\n\n". 
+                      "Met vriendelijke groet,\n". 
+                      "admin";
+          */
+          $messageHtml = "<!DOCTYPE html>
+                          <html>
+                            <head>
+                                <title>Page Title</title>
+                                <style>
+                                  body
+                                  {
+                                      font-family: Verdana, Arial;
+                                      font-size: 1em;
+                                      color: rgb(30, 30, 30);
+                                  }
+                                </style>
+                            </head>
+                            <body>
+                            <h3>Beste ".$firstname." ".$infix." ".$lastname.",</h3>".
+                                "<p>Bedankt voor het registreren, klik op onderstaande link<p>".
+                                "<p><a href='http://localhost/2016-2017/am1a/inlogregistratiesysteem/index.php?content=activate&id=".
+                                $last_id."&pw=".$tempPassword."'>activatielink</a></p><p>om uw account te activeren</p>". 
+                                "<p>Met vriendelijke groet,</p>". 
+                                "<p>admin</p>
+                            </body>
+                          </html>";
+          
+          $headers = "Content-Type: text/html; charset=UTF-8"."\r\n";
+          $headers .= "Cc: adruijter@fopmail.com, hans@testmail.com, frans@realmail.com"."\r\n";
+          $headers .= "Bcc: rra@mboutrecht.nl"."\r\n";
+          $headers .= "From: adruijter@gmail.com";
 
 
 
-         mail($emailaddress, $subject, $messageHtml, $headers);
-         // Geef een succes melding en stuur door naar de homepage..
-         echo "Uw bent geregistreerd";
-         header("refresh:3; url=./index.php?content=home");
+          mail($emailaddress, $subject, $messageHtml, $headers);
+          // Geef een succes melding en stuur door naar de homepage..
+          echo "Uw bent geregistreerd";
+          // Wacht 3 seconden en stuur door naar index.php?content=home
+          header("refresh:3; url=./index.php?content=home");
+        }
+        else
+        {
+           /* Wanneer de query niet goed is ontvangen of uitgevoerd, meldt dit 
+           * dan en stuur door naar het registratieformulier */
+           echo "Registreer opnieuw, er is iets misgegaan";
+           header("refresh:3; url=./index.php?content=register_form");
+        }
       }
       else
       {
-         /* Wanneer de query niet goed is ontvangen of uitgevoerd, meldt dit 
-          * dan en stuur door naar het registratieformulier */
-         echo "Registreer opnieuw, er is iets misgegaan";
-         header("refresh:3; url=./index.php?content=register_form");
+          // Wanneer het e-mailadres al bestaat worden we doorgestuurd naar de registratiepagina.
+          echo "Uw ingevoerde e-mailadres is al bekent in onze database. Kies een nieuw e-mailadres";
+          // Wacht 4 seconden en stuur door naar index.php?content=register_form
+          header("refresh: 4; url=index.php?content=register_form");
       }
    }
    // Wanneer er nog op de submitknop is gedrukt dan wordt het registratieformulier getoond
