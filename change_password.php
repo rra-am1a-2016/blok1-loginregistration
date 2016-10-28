21fc106a8a68410948881972f059641ff2f17029
Arjan de Ruijter <rra@mboutrecht.nl>
2016-10-28 11:52:34 +0200

    twee opdrachten

---
+++ change_password.php
@@ -0,0 +1,9 @@
+<?php
+// Maak een in tabel zichtbaar de velden
+// Voornaam tussenvoegsel en achternaam en een potloodicon
+// Wanneer er op het potloodicon wordt geklikt zie je twee password velden
+// en een submit knop.
+// Na het invullen van twee gelijke passwords wordt het password veranderd in de database
+// Wanneer de ingevulde passwords ongelijk zijn moet je het opnieuw proberen.
+
+?>
